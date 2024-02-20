4f98b7b5d802959e4d172e027acacfd3d34435b9
rubenskuhn <rubenskuhn@gmail.com>
2024-02-20 16:58:50 +0100

    vercel revenge

--- pages/api/[id]/index.js
+++ pages/api/[id]/index.js
@@ -1,5 +1,5 @@
-import dbConnect from "../../../../db/connect.js";
-import question from "../../../../db/models/Question.js";
+import dbConnect from "../../../db/connect";
+import Question from "../../../db/models/Question";
 
 export default async function handler(request, response) {
   const { id } = request.query;
@@ -11,23 +11,19 @@ export default async function handler(request, response) {
   await dbConnect();
 
   if (request.method === "GET") {
-    const question = await question.findById(id);
+    const question = await Question.findById(id);
+    if (!question) {
+      return response.status(404).json({ status: "Not found" });
+    }
     return response.status(200).json(question);
   } else if (request.method === "PUT") {
-    await question.findByIdAndUpdate(id, {
+    await Question.findByIdAndUpdate(id, {
       $set: request.body,
     });
   }
-
-  //   if (request.method === "DELETE") {
-  //     const deleteEntryPlace = await Place.findByIdAndDelete(id);
-  //     await Comment.deleteOne({ _id: { $in: deleteEntryPlace.comments } });
-  //     response.status(201).json("Entry Deleted");
-  //   }
-
-  if (!question) {
-    return response.status(404).json({ status: "Not found" });
-  }
-
-  response.status(200).json({ question: question });
 }
+//   if (request.method === "DELETE") {
+//     const deleteEntryPlace = await Place.findByIdAndDelete(id);
+//     await Comment.deleteOne({ _id: { $in: deleteEntryPlace.comments } });
+//     response.status(201).json("Entry Deleted");
+//   }
