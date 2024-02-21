403662cb18aa64ae3da0bb4daa1b61449a55de0a
rubenskuhn <rubenskuhn@gmail.com>
2024-02-21 15:24:56 +0100

    final touch

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9316bf8f2c48fe2d"; }
+/******/ 		__webpack_require__.h = function() { return "55bc575e77281e56"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
